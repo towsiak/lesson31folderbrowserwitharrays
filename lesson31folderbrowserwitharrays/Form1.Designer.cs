4e3d0d263799d71f7c43f321fc31054452f5248b
Tom Owsiak <towsiak@yahoo.com>
2016-11-13 13:03:08 -0500

    lessons 31/32 files

---
+++ lesson31folderbrowserwitharrays/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace lesson31folderbrowserwitharrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnListFiles = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.txtFolderPath.Location = new System.Drawing.Point(13, 13);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(1091, 53);
+            this.txtFolderPath.TabIndex = 0;
+            this.txtFolderPath.DoubleClick += new System.EventHandler(this.txtFolderPath_DoubleClick);
+            // 
+            // btnListFiles
+            // 
+            this.btnListFiles.Enabled = false;
+            this.btnListFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btnListFiles.Location = new System.Drawing.Point(12, 81);
+            this.btnListFiles.Name = "btnListFiles";
+            this.btnListFiles.Size = new System.Drawing.Size(214, 60);
+            this.btnListFiles.TabIndex = 1;
+            this.btnListFiles.Text = "List Files";
+            this.btnListFiles.UseVisualStyleBackColor = true;
+            this.btnListFiles.Click += new System.EventHandler(this.btnListFiles_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 157);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1091, 515);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 694);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnListFiles);
+            this.Controls.Add(this.txtFolderPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnListFiles;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ lesson31folderbrowserwitharrays/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;//allows using Directory class so we can get the all the files in a folder
+
+namespace lesson31folderbrowserwitharrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFolderPath_DoubleClick(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();//when user double clicks in box, a folder browser shows
+            txtFolderPath.Text = folderBrowserDialog1.SelectedPath;//puts selected path in text box
+            btnListFiles.Enabled = true;//this makes our button enabled
+        }
+
+        private void btnListFiles_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";//clear text box on each button click so output does not accumulate
+            //we're going to create an array
+            //an array is a collection of items
+            //array values can be examined one at a time using a foreach loop
+            //Directory.GetFiles returns an array of file names, which is then saved to the files array
+            string[] files = Directory.GetFiles(txtFolderPath.Text);
+            foreach (string file in files)//grab each file name, store it to the variable file
+                richTextBox1.Text += $"{file}\n";//display the files in the rich text
+        }
+
+    }
+}
